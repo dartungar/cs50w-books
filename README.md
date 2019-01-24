58bd80c9b91a9027839113fac5afd0d0b664321b
dartungar <dartungar@gmail.com>
2019-01-24 15:02:26 +0300

    create tables; readme

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# Project 1
+# Dartungar's Books: CS50W Project 1
 
-Web Programming with Python and JavaScript
+This is a simple project for CS50W: a site where you can look up & review books.
+
+
+
+* application.py - main application file.
+* import.py - a simple script for importing from csv to SQL.
+* books.csv - small dataset for testing.
+
+pages:
+* search - look up books by ISBN, title or author name. view search results (clickable). login required
+* book - view details about the book, reviews, and leave your own review. login required
+* login - log in.
+* register - register.
--- application.py
+++ application.py
@@ -1,6 +1,8 @@
-import functools, requests, os
+import functools
+import requests
+import os
 
-from flask import Flask, abort, flash, jsonify, g, session, redirect, render_template, request, url_for
+from flask import Flask, abort, flash, jsonify, session, redirect, render_template, request, url_for
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from sqlalchemy import create_engine
@@ -24,6 +26,37 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+# prepare tables
+db.execute("""CREATE TABLE IF NOT EXISTS
+              users
+              (
+                id SERIAL PRIMARY KEY,
+                username VARCHAR(15) NOT NULL,
+                password VARCHAR(100) NOT NULL
+              ) """)
+
+db.execute("""CREATE TABLE IF NOT EXISTS
+              reviews
+              (
+                id SERIAL PRIMARY KEY,
+                book_isbn VARCHAR(15) NOT NULL REFERENCES books(isbn),
+                author_id INT NOT NULL REFERENCES users(id),
+                text VARCHAR(1000) NOT NULL,
+                rating INT NOT NULL,
+                UNIQUE (book_isbn, author_id)
+              ) """)
+
+db.execute("""CREATE TABLE IF NOT EXISTS
+              books
+              (
+                id SERIAL PRIMARY KEY,
+                isbn VARCHAR(15) UNIQUE NOT NULL,
+                title VARCHAR(300),
+                author VARCHAR(100),
+                year INT
+              ) """)
+
+db.commit()
 
 # wrapper for checking if user is logged in
 def login_required(view):
@@ -31,9 +64,9 @@ def login_required(view):
     def wrapped_view(**kwargs):
         if session.get('user_id') is None:
             return redirect(url_for('login'))
-        
+
         return view(**kwargs)
-    
+
     return wrapped_view
 
 
@@ -64,30 +97,38 @@ def book(isbn):
 
         review_rating = request.form['review-rating']
         review_text = request.form['review-text']
-        
+
         db.execute("""INSERT INTO reviews (rating, text, book_isbn, author_id)
                     VALUES (:rating, :text, :book_isbn, :author_id)
+                    ON CONFLICT DO NOTHING
                     """,
                     {"rating": review_rating,
                     "text": review_text,
                     "book_isbn": isbn,
                     "author_id": session["user_id"]}
-                    )
+                   )
         db.commit()
 
     # get book info from our DB
-    book = db.execute("SELECT isbn, title, author, year FROM books WHERE isbn = :isbn", {"isbn": isbn}).fetchone()
+    book = db.execute("""SELECT isbn, title, author, year
+                         FROM books WHERE isbn = :isbn""",
+                         {"isbn": isbn}).fetchone()
 
-    reviews = db.execute("""SELECT users.username, reviews.rating, reviews.text 
-                        FROM reviews INNER JOIN users ON reviews.author_id = users.id 
-                        WHERE reviews.book_isbn = :isbn""", {"isbn": isbn}).fetchall()
+    reviews = db.execute("""SELECT users.username, reviews.rating, reviews.text
+                        FROM reviews
+                        INNER JOIN users ON reviews.author_id = users.id
+                        WHERE reviews.book_isbn = :isbn""",
+                        {"isbn": isbn}).fetchall()
 
     # get additional book info from GoodReads API
-    res = requests.get('https://www.goodreads.com/book/review_counts.json', 
-                       params={"key": os.getenv('GOODREADS_API_KEY'), "isbns": isbn}
+    res = requests.get('https://www.goodreads.com/book/review_counts.json',
+                       params={
+                           "key": os.getenv('GOODREADS_API_KEY'), 
+                           "isbns": isbn
+                           }
                        )
-    gr_data = res.json()['books'][0] 
-    
+    gr_data = res.json()['books'][0]
+
     # check if this user already reviewed the book
     # user can leave only 1 review
     already_reviewed = db.execute("""SELECT reviews.text FROM reviews
@@ -109,11 +150,13 @@ def api(isbn):
     # prepare keys & fetch values for data
     keys = ['title', 'author', 'year', 'isbn', 'review_count', 'average_score']
     values = db.execute("""SELECT
-            books.title, books.author, books.year, books.isbn, COUNT(reviews.rating), AVG(reviews.rating)
+            books.title, books.author, books.year, books.isbn,
+            COUNT(reviews.rating), AVG(reviews.rating)
             FROM books
-            LEFT JOIN reviews ON books.id = reviews.book_id
+            LEFT JOIN reviews ON books.isbn = reviews.book_isbn
             WHERE books.isbn=:isbn
-            GROUP BY books.title, books.author, books.year, books.isbn""", {"isbn": isbn}).fetchone()
+            GROUP BY books.title, books.author, books.year, books.isbn""",
+            {"isbn": isbn}).fetchone()
 
     # if isbn is in our DB, build container, JSONify & return in
     if values:
@@ -127,7 +170,7 @@ def api(isbn):
 # almost as in Flask documentation
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == "POST": 
+    if request.method == "POST":
 
         username = request.form['username']
         password = request.form['password']
@@ -144,7 +187,9 @@ def register():
         if error is None:
             db.execute(
                 'INSERT INTO users (username, password) VALUES (:username, :password_hash)',
-                        {'username': username, 'password_hash': generate_password_hash(password)}
+                {'username': username,
+                    'password_hash': generate_password_hash(password)
+                 }
                     )
             db.commit()
             flash(f'User {username} registered.')
@@ -163,7 +208,8 @@ def login():
         username = request.form['username']
         password = request.form['password']
         error = None
-        user = db.execute("SELECT * FROM users WHERE username = :username", {"username": username}).fetchone()
+        user = db.execute("SELECT * FROM users WHERE username = :username", 
+                          {"username": username}).fetchone()
 
         if user is None:
             error = 'Invalid username.'
